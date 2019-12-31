2efcfa2e51e5ba09f71dbdb6da6a30a404c0bc08
Victor Adrian Reyes <victor.reyez@cmv.mx>
2019-12-31 09:30:19 -0600

    main

--- app/src/main/java/com/example/mibanco/MainActivity.java
+++ app/src/main/java/com/example/mibanco/MainActivity.java
@@ -68,7 +68,6 @@ public class MainActivity extends AppCompatActivity
                                          }catch (Exception ex){
                                              Log.d("",ex.getMessage());
                                          }
-
                                      }
                                  }
 
